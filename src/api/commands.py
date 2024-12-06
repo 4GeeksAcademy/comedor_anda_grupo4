6940ab83f5512335afefd25cb7ac702d85d3c13e
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2024-12-06 22:17:50 +0000

    :recycle: refactor: remove obsolete migration and update product model with constructor

--- src/api/commands.py
+++ src/api/commands.py
@@ -1,7 +1,7 @@
 
 import click
-from api.models import db, User
-
+from api.models import db, User, Product
+import random
 """
 In this file, you can add as many commands as you want using the @app.cli.command decorator
 Flask commands are usefull to run cronjobs or tasks outside of the API but sill in integration 
@@ -35,3 +35,61 @@ def setup_commands(app):
     @app.cli.command("insert-test-data")
     def insert_test_data():
         pass
+
+    def check_name_already_exists(name):
+        return Product.query.filter_by(name=name).first() is not None
+
+    images = [
+        "https://res.cloudinary.com/dd0wschpy/image/upload/v1733522394/dectada-pasta_tfy9x6.webp",
+        "https://res.cloudinary.com/dd0wschpy/image/upload/v1733522394/milanesa_de_carne_11894_orig_mgs2gi.jpg",
+        "https://res.cloudinary.com/dd0wschpy/image/upload/v1733522394/20231120-WEB-Lasanga-6422_ebwupb.webp",
+        "https://res.cloudinary.com/dd0wschpy/image/upload/v1733522393/65a016f48ab83_n25azc.jpg"
+    ]
+    product_types = ['Ejecutivo', 'Bebidas', 'Minutas']
+
+    name_subj = ['Milanesa', 'Pasta', 'Lasagna', 'Pizza', 'Ensalada', 'Hamburguesa', 'Empanadas', 'Tarta', 'Sushi', 'Wok']
+    name_compl = ['de carne', 'de pollo', 'de verdura', 'de jamon y queso', 'de atun', 'de salmon', 'de espinaca', 'de calabaza', 'de berenjena', 'de zapallitos']
+
+    descriptions = [
+        f'{random.choice(name_subj)} {random.choice(name_compl)} con guarnición de {random.choice(["papas fritas", "ensalada", "arroz", "puré"])}',
+        f'{random.choice(name_subj)} con salsa de {random.choice(["tomate", "queso", "blanca", "roquefort"])} y {random.choice(["queso rallado", "jamón", "champiñones", "aceitunas"])}',
+        f'{random.choice(name_subj)} con {random.choice(["salsa blanca", "salsa roja", "salsa de champiñones", "salsa de espinaca"])} y {random.choice(["tomate", "queso", "jamón", "pollo"])}',
+        f'{random.choice(name_subj)} de {random.choice(["jamón", "morrón", "queso", "anchoas"])}',
+        f'Ensalada de {random.choice(["lechuga", "tomate", "huevo", "zanahoria", "pepino"])}',
+        f'Hamburguesa con {random.choice(["queso", "panceta", "huevo", "cebolla caramelizada"])}',
+        f'Empanadas de {random.choice(["carne", "pollo", "verdura", "jamón y queso"])}',
+        f'Tarta de {random.choice(["jamón y queso", "verdura", "pollo", "calabaza"])}',
+        f'Sushi de {random.choice(["salmón", "atún", "langostinos", "vegetales"])}',
+        f'Wok de {random.choice(["verduras", "pollo", "carne", "camarones"])}'
+    ]
+    def insert_random_product():
+        name = f'{random.choice(name_subj)} {random.choice(name_compl)}'
+        description = random.choice(descriptions)
+        price = round(random.uniform(150.0, 500.0), 2)
+        image = random.choice(images)
+        type = random.choice(product_types)
+
+        if check_name_already_exists(name):
+            print(f'Product {name} already exists. Skipping...')
+            return False
+
+        new_product = Product(
+            type=type,
+            name=name,
+            description=description,
+            stock=random.randint(1, 50),
+            image=image,
+        )
+
+        db.session.add(new_product)
+        db.session.commit()
+        return True
+
+    @app.cli.command("populate-products")
+    def insert_random_products():
+        count = 0
+        while count < 10:
+            if insert_random_product():
+                count += 1
+                print(f'Product {count} created.')
+            
